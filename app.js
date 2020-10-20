f4994ed45cece878f9e10097a530314cc7874e13
ahmed tammam <tamam65@gmail.com>
2020-10-20 05:52:39 +0200

    build Auth routes, controller and user model

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+
+const authRoutes = require("./routes/auth");
+
+// DB
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log("DB connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+// middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+// routes Middleware
+app.use("/api", authRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
---
+++ controllers/auth.js
@@ -0,0 +1,57 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+exports.signup = (req, res) => {
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    user.salt = undefined;
+    user.hashed_password = undefined;
+    res.json({ user });
+  });
+};
+
+exports.signin = (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "This Email isn't exist",
+      });
+    }
+
+    if (!user.authenticated) {
+      return res.status(401).json({
+        error: "Email and password not match",
+      });
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_TOKEN);
+    res.cookie("access_token", token, { exipre: new Date() + 9999 });
+
+    const { _id, name, email, role } = user;
+    return res.status(200).json({
+      token,
+      user: { _id, name, email, role },
+    });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("access_token");
+  res.json({ message: "Sigout success" });
+};
+
+//  will use with private routes
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_TOKEN,
+  algorithms: ["HS256"],
+  userProperty: "auth",
+});
