574c1bb5ed9c349220e9ded4599c9088858480c4
ahmed tammam <tamam65@gmail.com>
2020-10-22 19:03:27 +0200

    handle server can't run err

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const createServer = require("./server"); // new
 
 const db = require("./db/connection");
 
-db.connect().then(() => {
-  const app = createServer(); // new
-  const port = process.env.PORT || 8000;
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+db.connect()
+  .then(() => {
+    const app = createServer(); // new
+    const port = process.env.PORT || 8000;
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Server can't run: ${err}`);
   });
-});
